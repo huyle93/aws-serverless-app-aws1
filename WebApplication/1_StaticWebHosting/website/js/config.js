1705dd0e587664c0fe488dc5589fc0f14c092df9
Huy Le <huyle.finance@gmail.com>
2018-02-28 21:22:46 -0500

    log-in auth works, config.js added to s3 for userpool

--- WebApplication/1_StaticWebHosting/website/js/config.js
+++ WebApplication/1_StaticWebHosting/website/js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_kBYZ4udiW', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5cdff3c1hp8upoej389nbil8sc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
